cb6be673eb03cbedcbaf87af44b1b1489e1e4a91
henrique-roldao <henriqueroldao@rede.ulbra.br>
2021-03-05 11:33:45 -0300

    :tada: Dark and Light Mode

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) => 
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
+
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    textColor: getStyle(html, "--text-color"),
+}
+
+const lightMode = {
+    bg: "#F8F7FF",
+    textColor: "#202026"
+}
+
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key => 
+        html.style.setProperty(transformKey(key), colors[key]) 
+    )
+}
+
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors(lightMode) : changeColors(initialColors)
+})
